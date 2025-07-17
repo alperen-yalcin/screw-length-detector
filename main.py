40b127b679c3d6d3e693eccc5bbb9ae0310c3305
Arge Stajyer <argestajyer1@ferel.local>
2025-07-17 11:08:36 +0300

    Feat: Apply Gaussian blur and Canny edge detection

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import numpy as np
 import matplotlib.pyplot as lt
 image = cv2.imread('screw.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
+edges = cv2.Canny(blur, 50, 150)
