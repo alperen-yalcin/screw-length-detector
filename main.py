ecc7052da1994c4b81208b2547401632376ebe38
Arge Stajyer <argestajyer1@ferel.local>
2025-07-18 09:41:08 +0300

    Refactor: main file was created

---
+++ main.py
@@ -0,0 +1,3 @@
+import cv2
+from utils.edge_detection import edge_detection  
+edge_detection()
--- utils/edge_detection.py
+++ utils/edge_detection.py
@@ -1,30 +1,31 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-image = cv2.imread('screw.png')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def edge_detection():
+    image = cv2.imread('assets/screw.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-edges = cv2.Canny(blur, 50, 150)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blur, 50, 150)
 
-contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-largest_contour = max(contours, key = cv2.contourArea)
+    largest_contour = max(contours, key = cv2.contourArea)
 
-leftmost = tuple(largest_contour[largest_contour[:, :, 0].argmin()][0])
-rightmost= tuple(largest_contour[largest_contour[:, :, 0].argmax()][0])
+    leftmost = tuple(largest_contour[largest_contour[:, :, 0].argmin()][0])
+    rightmost= tuple(largest_contour[largest_contour[:, :, 0].argmax()][0])
 
-screw_length = np.linalg.norm(np.array(rightmost) - np.array(leftmost))
+    screw_length = np.linalg.norm(np.array(rightmost) - np.array(leftmost))
 
-output = image.copy()
-cv2.circle(output, leftmost, 5, (0, 255, 0), -1)
-cv2.circle(output, rightmost, 5, (0, 0, 255), -1)
-cv2.line(output, leftmost, rightmost, (255, 0, 0), 2)
+    output = image.copy()
+    cv2.circle(output, leftmost, 5, (0, 255, 0), -1)
+    cv2.circle(output, rightmost, 5, (0, 0, 255), -1)
+    cv2.line(output, leftmost, rightmost, (255, 0, 0), 2)
 
-print(f'Length of the screw (pixel): {screw_length:.2f}')
-print(f'Rightmost point coordinate: {rightmost}')
+    print(f'Length of the screw (pixel): {screw_length:.2f}')
+    print(f'Rightmost point coordinate: {rightmost}')
 
-plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
-plt.title('Measurement On The Screw')
-plt.axis('off')
-plt.show()
+    plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
+    plt.title('Measurement On The Screw')
+    plt.axis('off')
+    plt.show()
